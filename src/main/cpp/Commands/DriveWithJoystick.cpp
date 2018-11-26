f84916477edee390a7a31d7708cce23effb1b976
parsa-salimi <33131203+parsa-salimi@users.noreply.github.com>
2018-11-26 15:41:43 -0500

    alpha vision code

--- src/main/cpp/Commands/DriveWithJoystick.cpp
+++ src/main/cpp/Commands/DriveWithJoystick.cpp
@@ -1,6 +1,7 @@
 #include "Commands/DriveWithJoystick.h"
 
 #include "Robot.h"
+bool vision = true;
 
 DriveWithJoystick::DriveWithJoystick() {
   // Use Requires() here to declare subsystem dependencies
@@ -14,6 +15,8 @@ void DriveWithJoystick::Initialize() {
   this->directionMultiplier = 1;
   this->speedMultiplier     = 1;
 
+ //set up table for vision
+  table = NetworkTable::GetTable("SmartDashboard");
   //set Speed and Rotation
   this->speed    = 0.0;
   this->rotation = 0.0;
@@ -31,6 +34,12 @@ void DriveWithJoystick::Execute() {
 
   this->speed    = (this->speed * this->speedMultiplier * this->directionMultiplier);
   this->rotation = (this->rotation * this->directionMultiplier);
+	
+  //get speed and angle from networktables
+  if(vision) {
+	  this->speed = table->GetNumber("DistanceToCube",0.00) / 100);
+	  this->rotation = table->GetNumber("AngleToCube,0.00) / 100);
+  }	
 
   Robot::m_DriveTrain->ArcadeDrive(this->speed, this->rotation);
 }
