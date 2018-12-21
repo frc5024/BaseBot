d7b46e6e46f2bae5590e3a935aa9758260b2029a
Evan Pratten <ewpratten@gmail.com>
2018-12-21 12:28:46 -0500

    modified encoding

--- src/main/include/Telemetry.h
+++ src/main/include/Telemetry.h
@@ -10,21 +10,21 @@ namespace Telemetry{
 		
 		typedef enum RobotMode{ //!< Current mode of the robot
 			Auto     = 0,
-			Teleop   = 1,
-			Practice = 2,
-			Test     = 3
+			Teleop   = 10,
+			Practice = 20,
+			Test     = 30
 		} RobotMode;
 		
 		typedef enum RIO{ //!< Current state of the RoboRIO
 			Unready = 0,
-			Ready   = 1,
-			Fault   = 2
+			Ready   = 100,
+			Fault   = 200
 		} RIO;
 		
 		typedef enum RPI{ //!< Current state of the RaspberryPI
 			Unready = 0,
-			Ready   = 1,
-			Fault   = 2
+			Ready   = 1000,
+			Fault   = 2000
 		} RPI;
 	}
 	
@@ -40,8 +40,14 @@ namespace Telemetry{
 			void SetState(ControlData::ControlValue state);
 		
 		private:
-			int controlData[4]Q;     //!< Array of ints for storing data
+			int controlData[4];     //!< Array of ints for storing data
 			int lastControlData[4]; //!< Array to contain the last set of data sent over the network
+			
+			double packagedData = 0.00; //!< Double for storing the encoded data
+			
+			// Byte map
+			// |   1   |   1  |  1  |  1  |    0   |    0   |    0   |    0   |
+			// | state | mode | rio | rpi | unused | unused | unused | unused |
 	}
 	
 }
